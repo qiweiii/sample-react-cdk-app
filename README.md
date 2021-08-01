fe8bdc744509b4563747d70165dd9c256b0d9790
qiweiii <yangqiwei97@gmail.com>
2021-08-01 16:36:45 +0800

    add cognito

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 
+The React app is currently deployed here: https://main.d2mwro2ekf97im.amplifyapp.com/
+
 ## Build React app
 
 ```sh
@@ -18,4 +20,6 @@ cdk deploy
 ## Useful links
 
 - [deploying-a-static-website-with-aws-amplify-and-cdk](https://aws.amazon.com/blogs/mobile/deploying-a-static-website-with-aws-amplify-and-cdk/)
-- [react bootstrap](https://react-bootstrap.github.io/components/alerts/)
+- [https://github.com/drewword/cognito-cdk-amplify](https://github.com/drewword/https://github.com/drewword/cognito-cdk-amplify)
+- [amazon-cognito-example-for-external-idp](https://github.com/aws-samples/amazon-cognito-example-for-external-idp)
+- [amplify-auth](https://docs.amplify.aws/lib/auth/start/q/platform/js#create-new-authentication-resource)
--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,11 +1,24 @@
 import * as cdk from '@aws-cdk/core';
 import * as amplify from "@aws-cdk/aws-amplify";
+import * as cognito from '@aws-cdk/aws-cognito';
+import { CfnOutput } from '@aws-cdk/core';
+import * as iam from "@aws-cdk/aws-iam";
+
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // define Amplify
+
+    // ========================================================================
+    // Resource: Amplify App
+    // ========================================================================
+
+    // Purpose: creates an amplify frontend app
+    
+    // See also:
+    // - https://docs.aws.amazon.com/amplify/latest/userguide/welcome.html
+
     const amplifyApp = new amplify.App(this, "sample-react-cdk-app ", {
       sourceCodeProvider: new amplify.GitHubSourceCodeProvider({
         owner: "qiweiii",
@@ -13,10 +26,89 @@ export class InfraStack extends cdk.Stack {
         oauthToken: cdk.SecretValue.secretsManager("sample-react-cdk-app-github", {
           jsonField: "github_access_token", 
           // note: i set github_access_token on github to expire in 90 days, 
-          // so rmb to re-create it and add to aws later on
+          // so rmb to re-create it and add to aws secretsManager again later on
         }),
       }),
     });
     const masterBranch = amplifyApp.addBranch("main");
+
+
+    // ========================================================================
+    // Resource: Amazon Cognito User Pool
+    // ========================================================================
+
+    // Purpose: creates a user directory and allows federation from external IdPs
+
+    // See also:
+    // - https://aws.amazon.com/cognito/
+    // - https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-cognito.CfnIdentityPool.html
+
+    // high level construct
+    const userPool: cognito.UserPool = new cognito.UserPool(this, id + "srca-UserPool", {
+      selfSignUpEnabled: true, // Allow users to sign up
+      autoVerify: { email: true }, // Verify email addresses by sending a verification code
+      signInAliases: { email: true }, // Set email as an alias
+    });
+
+    // any properties that are not part of the high level construct can be added using this method
+    const userPoolCfn = userPool.node.defaultChild as cognito.CfnUserPool;
+    userPoolCfn.userPoolAddOns = { advancedSecurityMode: "ENFORCED" }
+    userPoolCfn.schema = [{
+      name: "userPool",
+      attributeDataType: "String",
+      mutable: true,
+      required: false,
+      stringAttributeConstraints: {
+        maxLength: "2000"
+      }
+    }];
+
+    // create two groups, one for admins one for users
+    // these groups can be used without configuring a 3rd party IdP
+    new cognito.CfnUserPoolGroup(this, "srca-AdminsGroup", {
+      groupName: 'admin',
+      userPoolId: userPool.userPoolId,
+    });
+
+    new cognito.CfnUserPoolGroup(this, "srca-UsersGroup", {
+      groupName: 'users',
+      userPoolId: userPool.userPoolId,
+    });
+
+    const userPoolClient = new cognito.UserPoolClient(this, "srca-UserPoolClient", {
+      userPool,
+      generateSecret: false, // Don't need to generate secret for web app running on browsers
+    });
+
+
+    // ========================================================================
+    // Resource: Amazon Cognito Identity Pool
+    // ========================================================================
+
+    // Purpose: provide temporary AWS credentials for users who are guests (unauthenticated)
+    // and for users who have been authenticated and received a token.
+    // - https://docs.aws.amazon.com/cognito/latest/developerguide/identity-pools.html
+    
+    const identityPool = new cognito.CfnIdentityPool(this, "srca-IdentityPool", {
+      allowUnauthenticatedIdentities: false, // Don't allow unathenticated users
+      cognitoIdentityProviders: [
+        {
+          clientId: userPoolClient.userPoolClientId,
+          providerName: userPool.userPoolProviderName,
+        },
+      ],
+    });
+
+    
+    // Export values
+    new CfnOutput(this, "UserPoolId", {
+      value: userPool.userPoolId,
+    });
+    new CfnOutput(this, "UserPoolClientId", {
+      value: userPoolClient.userPoolClientId,
+    });
+    new CfnOutput(this, "IdentityPoolId", {
+      value: identityPool.ref,
+    });
   }
 }
